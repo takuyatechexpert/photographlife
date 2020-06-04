865ffa2472d08400455052d72c49ecf183e14e10
takuyatechexpert <lp4546.sg.abc@gmail.com>
2020-06-04 18:22:56 +0900

    add toppage controller
    TopPageに必要なcontrollerを生成
    TopPageの表示のためのルーティング設定

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', 'TopPageController@index')->name('toppage.index');
 
 Auth::routes();
 
